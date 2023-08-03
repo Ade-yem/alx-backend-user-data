0ee731853bc8e9f35355fb6b1795d2f7f4f8cad0
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-08-04 00:50:16 +0100

    all

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Regex-ing"""
+
+import re
+from typing import List
+import logging
+
+
+def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(field + "=.+?" + separator,
+                         field + "=" + redaction + separator, message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Constructor method"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+def get_logger() -> logging.Logger:
+    """returns a logging.Logger object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(handler)
+    return logger
+
