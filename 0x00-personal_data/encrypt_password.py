3b34a9be564eb69ced816196bb2212b4a21b8a4d
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-08-04 01:38:44 +0100

    all functions completed

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Encrypting passwords"""
+
+import bcrypt
+from typing import ByteString
+
+
+def hash_password(password: str) -> ByteString:
+    """returns a salted, hashed password, which is a byte string"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """returns a boolean"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,13 @@
 import re
 from typing import List
 import logging
+import mysql.connector
+import os
+import bcrypt
 
 
-def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: list, redaction: str,
+                 message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
         message = re.sub(field + "=.+?" + separator,
@@ -31,7 +35,11 @@ class RedactingFormatter(logging.Formatter):
         """filter values in incoming log records"""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
 def get_logger() -> logging.Logger:
     """returns a logging.Logger object"""
     logger = logging.getLogger("user_data")
@@ -42,3 +50,37 @@ def get_logger() -> logging.Logger:
     logger.addHandler(handler)
     return logger
 
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ returns a connector to the database"""
+    PERSONAL_DATA_DB_USERNAME = os.getenv('PERSONAL_DATA_DB_USERNAME')
+    PERSONAL_DATA_DB_PASSWORD = os.getenv('PERSONAL_DATA_DB_PASSWORD')
+    PERSONAL_DATA_DB_HOST = os.getenv('PERSONAL_DATA_DB_HOST')
+    PERSONAL_DATA_DB_NAME = os.getenv('PERSONAL_DATA_DB_NAME')
+    cnx = mysql.connector.connection.MySQLConnection(
+        user=PERSONAL_DATA_DB_USERNAME,
+        password=PERSONAL_DATA_DB_PASSWORD,
+        host=PERSONAL_DATA_DB_HOST,
+        database=PERSONAL_DATA_DB_NAME
+    )
+    return cnx
+
+
+def main() -> None:
+    """entry"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    logger = get_logger()
+    for row in cursor:
+        message = "name={}; email={}; phone={}; ssn={}; password={}; ".format(
+            row[0], row[1], row[2], row[3], row[4], row[5])
+        message += "ip={}; last_login={}; user_agent={};".format(
+            row[5], row[6], row[7])
+        logger.info(message)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
