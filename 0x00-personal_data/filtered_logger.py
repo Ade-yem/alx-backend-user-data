440abf9e67847b5e0bc32769722f3e2bf4adeec1
Adeyemi Adejumo <93265896+Ade-yem@users.noreply.github.com>
2023-08-05 09:07:11 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,15 +6,14 @@ from typing import List
 import logging
 import mysql.connector
 import os
-import bcrypt
 
 
 def filter_datum(fields: List, redaction: str,
                  message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
-        message = re.sub(field + "=.*?" + separator,
-                         field + "=" + redaction + separator, message)
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
     return message
 
 
